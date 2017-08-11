60c98525668cf5290bcfbf09390734f3902d30cf
Mirage <yangkangxierong@gmail.com>
2017-08-11 18:08:23 +0800

    添加单向链表
    1 添加单向链表
    2 添加容器基础

--- Algorithm/Algorithm/Container/C/ContainerBase.h
+++ Algorithm/Algorithm/Container/C/ContainerBase.h
@@ -1,5 +1,68 @@
 #ifndef _CONTAINER_BASE
 #define _CONTAINER_BASE
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int BOOL;
+typedef void* PVOID;
+
+#define TRUE 1
+#define FALSE 1
+
+typedef struct ContainerNode
+{
+	void* container;
+	PContainerNode next;
+}ContainerNode, Container, *PContainerNode, *PContainer;
+
+PContainerNode AddContainerNode(PVOID nodeContent, PContainer container)
+{
+	PContainerNode pIterator;
+	PContainerNode pnode;
+	if (container == NULL || nodeContent == NULL)
+		return NULL;
+
+	pnode = (PContainerNode)malloc(sizeof(ContainerNode));
+	pnode->next = NULL;
+
+	if (container == NULL)
+		container = pnode;
+	else
+	{
+		pIterator = container;
+		while (pIterator->next != NULL) pIterator = pIterator->next;
+		pIterator->next = pnode;
+	}
+
+	return pnode;
+}
+
+BOOL RemoveContainerNode(PVOID nodeContent, PContainer container)
+{
+	PContainerNode pIterator = NULL;
+	PContainerNode pIteratorPre = NULL;
+	if (container == NULL || nodeContent ==NULL)
+		return FALSE;
+
+	pIterator = container;
+	pIteratorPre = container;
+	while (pIterator->next != NULL)
+	{
+		if (pIterator->container == nodeContent)
+		{
+			if (pIterator == container)
+				return FALSE;
+			pIteratorPre->next = pIterator->next;
+			free(pIterator);
+			return TRUE;
+		}
+		else
+			pIteratorPre = pIterator;
+		pIterator = pIterator->next;
+	}
+
+	return FALSE;
+}
 
 #endif //  _CONTAINER_BASE
--- Algorithm/Algorithm/Container/C/SinglyLinkedList.h
+++ Algorithm/Algorithm/Container/C/SinglyLinkedList.h
@@ -1,17 +1,39 @@
 #ifndef _SINGLY_LINKED_LIST
 #define _SINGLY_LINKED_LIST
 
-typedef struct SL_Node
+#include "ContainerBase.h"
+
+typedef struct SL_Node ///单向链表节点
 {
 	void* Context;
 	SL_Node* next;
-}SL_Node;
+}SL_Node, *PSLNode, *SLList;
 
-typedef SL_Node* SLList;
+Container sllistContainer;
 
 SLList CreateSLList()
 {
+	SLList list = (SLList)malloc(sizeof(SL_Node));
+	list->next = NULL;
+	AddContainerNode(list, &sllistContainer);
+	return list;
+}
 
+BOOL DeleteSLList(SLList list)
+{
+	RemoveContainerNode(list, &sllistContainer);
+}
+
+PSLNode AddSLListNode(PVOID nodeContent, SLList list)
+{
+	PSLNode node = (PSLNode)malloc(sizeof(SL_Node));
+	node->next = NULL;
+	node->Context = nodeContent;
+
+	while (list->next != NULL) list = list->next;
+	list->next = node;
+
+	return node;
 }
 
 #endif //  _SINGLY_LINKED_LIST
